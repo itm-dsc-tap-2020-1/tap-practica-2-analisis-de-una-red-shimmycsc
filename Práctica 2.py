26a09c15d0f032c48406283a0899d1c51e43e3ba
Irvin Daniel Escutia Arreola <dany_55@hotmail.es>
2020-02-27 11:56:09 -0600

    Create Práctica 2.py

---
+++ Práctica 2.py
@@ -0,0 +1,47 @@
+import os
+
+red= "200.33.171.0/24"
+os.system("nmap -sP " + red)
+
+"""
+Starting Nmap 7.60 ( https://nmap.org ) at 2020-02-27 11:45 CST
+Nmap scan report for mail.itmorelia.edu.mx (200.33.171.3)
+Host is up (0.094s latency).
+Nmap scan report for delfin2.itmorelia.edu.mx (200.33.171.11)
+Host is up (0.067s latency).
+Nmap scan report for 200.33.171.13
+Host is up (0.081s latency).
+Nmap scan report for dsc.itmorelia.edu.mx (200.33.171.20)
+Host is up (0.062s latency).
+Nmap scan report for libra.itmorelia.edu.mx (200.33.171.54)
+Host is up (0.098s latency).
+Nmap scan report for sagitario.itmorelia.edu.mx (200.33.171.65)
+Host is up (0.11s latency).
+Nmap scan report for 200.33.171.66
+Host is up (0.11s latency).
+Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+Host is up (0.088s latency).
+Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+Host is up (0.076s latency).
+Nmap scan report for 200.33.171.85
+Host is up (0.096s latency).
+Nmap scan report for 200.33.171.97
+Host is up (0.11s latency).
+Nmap scan report for 200.33.171.99
+Host is up (0.094s latency).
+Nmap scan report for 200.33.171.115
+Host is up (0.11s latency).
+Nmap scan report for 200.33.171.118
+Host is up (0.11s latency).
+Nmap scan report for vinculacion.itmorelia.edu.mx (200.33.171.124)
+Host is up (0.11s latency).
+Nmap scan report for 200.33.171.125
+Host is up (0.11s latency).
+Nmap scan report for 200.33.171.127
+Host is up (0.11s latency).
+Nmap scan report for 200.33.171.128
+Host is up (0.11s latency).
+Nmap scan report for 200.33.171.160
+Host is up (0.10s latency).
+Nmap done: 256 IP addresses (19 hosts up) scanned in 3.35 seconds
+"""
